daf95a358e283093beebb1c6ba5fc150087249b7
Lokie89 <traeuman@gmail.com>
2020-01-03 00:34:41 +0900

    [add] randomnumber

--- src/main/java/RandomNumber.java
+++ src/main/java/RandomNumber.java
@@ -1,8 +1,21 @@
+import java.util.Random;
+
 public class RandomNumber {
-    int digit;
+    final int digit;
+    StringBuilder sb;
 
     public RandomNumber(int digit) {
         this.digit = digit;
     }
-    
+
+    private String getRandomNumber(){
+        Random random = new Random();
+        int ranNum = random.nextInt(10);
+        return Integer.toString(ranNum);
+    }
+
+    public String getNumber(){
+
+    }
+
 }
